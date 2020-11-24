816136d2ba4871094f97d6a6bdaa38305c543370
Rogerio Colaço <rcs@ufscar.br>
2020-11-24 20:28:21 -0300

    Initial commit

---
+++ src/main/kotlin/colecoes.kt
@@ -0,0 +1,8 @@
+fun main(){
+    //Lista é imutável
+    var familia: List<String> = listOf("Rogerio", "Monica")
+
+    for (integrante in familia){
+        println(integrante)
+    }
+}
---
+++ src/main/kotlin/declaracoes.kt
@@ -0,0 +1,16 @@
+fun main(){
+   var nome : String = "Rogerio"
+
+   var sobrenome = "Colaco"
+
+   var ultimoNome: String
+   ultimoNome = "Silva"
+
+    val altura: Float = 1.76f
+
+    var idade: Int = 35
+
+    var peso: Double = 85.toDouble()
+
+    println("${nome} $sobrenome $ultimoNome, $altura, $idade, $peso")
+}
