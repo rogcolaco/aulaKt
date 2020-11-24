f1329ce1fc7b3ded08af87157fd92275f5646ea9
Rogerio Colaço <rcs@ufscar.br>
2020-11-24 20:36:40 -0300

    - declaração de colecoes

--- src/main/kotlin/colecoes.kt
+++ src/main/kotlin/colecoes.kt
@@ -5,4 +5,25 @@ fun main(){
     for (integrante in familia){
         println(integrante)
     }
+
+    println("\n")
+
+    val listaInteiros: List<Int> = listOf(1,2,3,4,5,6,7,8,9,10)
+    for (i in 0..9){
+        println(listaInteiros[i])
+    }
+
+    println("\n")
+
+    val listaInteiros2: List<Int> = (1..10).toList()
+    for (i in 0..9){
+        println(listaInteiros2[i])
+    }
+
+    println("\n")
+
+    val listaInteiros3: List<Int> = (1..10 step 2).toList()
+    for (i in 0 until listaInteiros3.size){
+        println(listaInteiros3[i])
+    }
 }
