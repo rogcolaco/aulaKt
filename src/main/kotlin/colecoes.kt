40d99bc826db2c2445ea5311445711fbdbed6d66
Rogerio Colaço <rcs@ufscar.br>
2020-11-24 21:21:27 -0300

    - mutableList, mutableSet, MutableMap

--- src/main/kotlin/colecoes.kt
+++ src/main/kotlin/colecoes.kt
@@ -6,21 +6,21 @@ fun main(){
         println(integrante)
     }
 
-    println("\n")
+    println()
 
     val listaInteiros: List<Int> = listOf(1,2,3,4,5,6,7,8,9,10)
     for (i in 0..9){
         println(listaInteiros[i])
     }
 
-    println("\n")
+    println()
 
     val listaInteiros2: List<Int> = (1..10).toList()
     for (i in 0..9){
         println(listaInteiros2[i])
     }
 
-    println("\n")
+    println()
 
     val listaInteiros3: List<Int> = (1..10 step 2).toList()
     for (i in 0 until listaInteiros3.size){
@@ -30,4 +30,31 @@ fun main(){
     listaInteiros3.forEach { x: Int ->  println("elemento: $x") }
     println("Imprimindo com forEach 2")
     listaInteiros3.forEach { println("elemento: $it") }
+
+    // Lista mutável
+    val listaCursos: MutableList<String> = mutableListOf("ADS", "TPG", "Matematica")
+    listaCursos.add("Biblioteconomia")
+
+    println()
+    listaCursos.forEach { println(it) }
+
+    // Set e mutableSet
+    val setCursos : MutableSet<String> = mutableSetOf("ADS", "SDM", "TII")
+    setCursos.add("ADS")
+    setCursos.add("SDM")
+    setCursos.add("TMA")
+    println()
+    setCursos.forEach{(println(it))}
+
+    //Map e MutableMap, HashMap
+    val familiaMap: MutableMap<String, String> = mutableMapOf(
+        Pair("Pai", "Nome do Pai"),
+        Pair("Mae", "Nome da Mae"),
+        Pair("Filho", "Nome do Filho")
+    )
+    //familiaMap.put("Cachorro", "Nome do Cachorro")
+    familiaMap["Cachorro"] = "Nome do Cachorro"
+
+    println()
+    familiaMap.keys.forEach { println("$it - ${familiaMap[it]}") }
 }
